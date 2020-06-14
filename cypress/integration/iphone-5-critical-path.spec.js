0d40ecf9cd8edea4af2430fbabb73436d2e733f2
Cecelia Martinez <cecelia@cypress.io>
2020-06-14 19:49:47 -0400

    speficies swipe test as only test to run and adds cy.pause() for demo purposes

--- cypress/integration/iphone-5-critical-path.spec.js
+++ cypress/integration/iphone-5-critical-path.spec.js
@@ -6,10 +6,11 @@ describe("critical path", () => {
         cy.viewport('iphone-5')
       })
 
-    it("swipes through tutorial", () => {
+    it.only("swipes through tutorial", () => {
+        //pauses added for demonstration purposes, can be removed
 
-        //pauses added for talk demo, can be removed
         cy.enableTutorial();
+        cy.pause()
         cy.swipeLeft();
         cy.pause()
         cy.swipeLeft();
@@ -17,5 +18,36 @@ describe("critical path", () => {
         cy.swipeLeft();
         cy.pause()
         cy.contains("Continue").click();
+    })
+
+    it("adds a session from list to favorites", () => {
+        cy.disableTutorial();
+        cy.contains('University of Ionic').click({force: true});
+        cy.get('[data-cy=favorite]').click();
+        cy.get('[data-cy=back').click();
+        cy.contains('Favorites').click();
+        cy.contains('University of Ionic');
+    })
+
+    it("enables dark mode", () => {
+
+        cy.disableTutorial();
+        cy.get('[data-cy=menu]').click();
+        cy.get('[data-cy=dark-mode]').click();
+        cy.contains("Dark Mode").should('have.css', 'color', 'rgb(255, 255, 255)')
+    })
+
+    it("searches for a session and adds to favorites", () => {
+
+        cy.disableTutorial();
+        cy.get('[data-cy=search]').click();
+        cy.get('[data-cy=searchbar]')
+        .click()
+        .type("Angular{enter}")
+        cy.contains("Angular Directives").click();
+        cy.get('[data-cy=favorite]').click();
+        cy.get('[data-cy=back]').click();
+        cy.contains("Favorites").click();
+        cy.contains("Angular Directives");
     })
 })
